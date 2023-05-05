978f947597cde48dacfad3343a2f1d0a3f6da99b
Bayra_ry03@itstep.edu.az <Bayra_ry03@itstep.edu.az>
2023-05-05 03:46:33 -0700

    Test

---
+++ PW on C++/№5/Task№5 calc/Task№5 calc/Task№5 calc.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int tmp = -1 + 2 * (1 + 5);
+	cout << tmp;
+	//char numbers[100]{};
+	//char total[100]{};
+	//cout << "Enter your numbers: ";
+	//cin >> numbers;
+	//int i = 0,k = 0;
+	//while (numbers[i] != '\0')
+	//{
+	//	if (numbers[i] == 42)
+	//	{
+	//		int tmp = (numbers[i - 1] - 48) * (numbers[i + 1] - 48);
+	//		total[k - 1] = tmp + 48;
+	//		i += 2;
+	//	}
+	//	else if (numbers[i] == 47)
+	//	{
+	//		int tmp = (numbers[i - 1] - 48) / (numbers[i + 1] - 48);
+	//		total[k - 1] = tmp + 48;
+	//		i += 2;
+	//	}
+	//	else
+	//	{
+	//		total[k] = numbers[i];
+	//		k++;
+	//		i++;
+	//	}
+	//}
+	//int result{};
+	//i = 0;
+	//while (total[i] != '\0')
+	//{
+	//	if (total[i] == 45)
+	//	{
+	//		int tmp = (total[i - 1] - 48) - (total[i + 1] - 48);
+	//		result += tmp;
+	//	}
+	//	else if (total[i] == 43)
+	//	{
+	//		int tmp = (total[i - 1] - 48) + (total[i + 1] - 48);
+	//		result += tmp;
+	//	}
+	//	i++;
+	//}
+	//cout << result;
+	return 0;
+}
