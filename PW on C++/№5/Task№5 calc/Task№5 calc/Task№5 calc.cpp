72754fe769fa718407ec4ef5b2bec444f9c4f97e
Bayra_ry03@itstep.edu.az <Bayra_ry03@itstep.edu.az>
2023-05-15 08:04:12 -0700

    Calc

--- PW on C++/№5/Task№5 calc/Task№5 calc/Task№5 calc.cpp
+++ PW on C++/№5/Task№5 calc/Task№5 calc/Task№5 calc.cpp
@@ -1,51 +1,108 @@
 ﻿#include <iostream>
 using namespace std;
+bool number(char symbol) {
+	return (symbol >= '0' && symbol <= '9');
+}
+int calculate(char op, int a, int b) {
+	switch (op) {
+	case '+':
+		return a + b;
+	case '-':
+		return a - b;
+	case '*':
+		return a * b;
+	case '/':
+		return a / b;
+	default:
+		return 0;
+	}
+}
+void index(char* arr, bool& test)
+{
+	for (size_t i = 0; i < arr[i] != '\0'; i++)
+	{
+		if ((arr[i] == '+' || arr[i] == '-' || arr[i] == '*' || arr[i] == '/') &&
+			(arr[i + 1] == '+' || arr[i + 1] == '-' || arr[i + 1] == '*' || arr[i + 1] == '/'))
+		{
+			cout << "\tWrong expression" << endl;
+			test = true;
+			break;
+		}
+		else if (arr[i] >= 58 || arr[i] <= 39)
+		{
+			cout << "\tWrong expression" << endl;
+			test = true;
+			break;
+		}
+		else if ((arr[0] >= 57 || arr[0] <= 48) && arr[0] != 40)
+		{
+			cout << "\tWrong expression" << endl;
+			test = true;
+			break;
+		}
+		else test = false;
+	}
+}
 int main()
 {
-	int tmp = -1 + 2 * (1 + 5);
-	cout << tmp;
-	//char numbers[100]{};
-	//char total[100]{};
-	//cout << "Enter your numbers: ";
-	//cin >> numbers;
-	//int i = 0,k = 0;
-	//while (numbers[i] != '\0')
-	//{
-	//	if (numbers[i] == 42)
-	//	{
-	//		int tmp = (numbers[i - 1] - 48) * (numbers[i + 1] - 48);
-	//		total[k - 1] = tmp + 48;
-	//		i += 2;
-	//	}
-	//	else if (numbers[i] == 47)
-	//	{
-	//		int tmp = (numbers[i - 1] - 48) / (numbers[i + 1] - 48);
-	//		total[k - 1] = tmp + 48;
-	//		i += 2;
-	//	}
-	//	else
-	//	{
-	//		total[k] = numbers[i];
-	//		k++;
-	//		i++;
-	//	}
-	//}
-	//int result{};
-	//i = 0;
-	//while (total[i] != '\0')
-	//{
-	//	if (total[i] == 45)
-	//	{
-	//		int tmp = (total[i - 1] - 48) - (total[i + 1] - 48);
-	//		result += tmp;
-	//	}
-	//	else if (total[i] == 43)
-	//	{
-	//		int tmp = (total[i - 1] - 48) + (total[i + 1] - 48);
-	//		result += tmp;
-	//	}
-	//	i++;
-	//}
-	//cout << result;
+	char input[100];
+	bool test = true;
+	while (test == true)
+	{
+		cout << "Enter your expression: ";
+		cin.getline(input, 100);
+		index(input, test);
+	}
+	int numArr[100]{};
+	char opArr[100]{};
+	int zero = 0;
+	int tmp = 0;
+	for (int i = 0; input[i] != '\0'; i++)
+	{
+		if (number(input[i]))
+		{
+			int num = input[i] - '0';
+			while (number(input[i + 1]))
+			{
+				num = num * 10 + (input[i + 1] - '0');
+				i++;
+			}
+			numArr[zero++] = num;
+		}
+		else if (input[i] == '+' || input[i] == '-' || input[i] == '*' || input[i] == '/')
+		{
+			while (tmp > 0 && (opArr[tmp - 1] == '*' || opArr[tmp - 1] == '/'))
+			{
+				int b = numArr[--zero];
+				int a = numArr[--zero];
+				char op = opArr[--tmp];
+				numArr[zero++] = calculate(op, a, b);
+			}
+			opArr[tmp++] = input[i];
+		}
+		else if (input[i] == '(')
+		{
+			opArr[tmp++] = '(';
+		}
+		else if (input[i] == ')')
+		{
+			while (tmp > 0 && opArr[tmp - 1] != '(')
+			{
+				int b = numArr[--zero];
+				int a = numArr[--zero];
+				char op = opArr[--tmp];
+				numArr[zero++] = calculate(op, a, b);
+			}
+			tmp--;
+		}
+	}
+	while (tmp > 0)
+	{
+		int b = numArr[--zero];
+		int a = numArr[--zero];
+		char op = opArr[--tmp];
+		numArr[zero++] = calculate(op, a, b);
+	}
+	cout << "Result: " << numArr[0] << endl;
 	return 0;
 }
